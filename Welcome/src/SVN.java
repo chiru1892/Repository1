f1dca03049787faf762c664ccc6310b56ae012d8
chiru1892 <chiru1892@gmail.com>
2016-05-12 19:38:30 -0700

    Commit4

--- Welcome/src/SVN.java
+++ Welcome/src/SVN.java
@@ -3,7 +3,7 @@ public class SVN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to SVN");
 	}
-	System.out.println("Welcome to SVN");
+	
 }
